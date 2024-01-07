27cf4fda9751cc25215f8aef478399aa23193313
Greg Breen <gregbree@amazon.com>
2024-01-07 17:14:50 +0800

    First cut

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # greengrass-ecowitt-weather
-AWS IoT Greengrass component for publishing Ecowitt data to AWS IoT Core
+
+AWS IoT Greengrass component for publishing Ecowitt weather data to AWS IoT Core
+
+## Compatibility
+
+Gets the Ecowitt weather "live data". This is command `0x27` in the EcoWitt Data Exchange TCP Protocol document.
+
+This component should be generally suitable for GW1000, 1100, 1900, 2000, 2001, 2680, 2650. However, it currently only parses and supports the live data items that are returned by the GW2000 (firmware V3.0.9) in my house. Trivial to modify it to suit your needs. This has been tested with 4xWH31, 1xWN36 and 3xWH51.
+
+## MQTT Messages
+
+Publishes once per minute to AWS IoT Core using the [AWS IoT Greengrass IPC mechanism](https://docs.aws.amazon.com/greengrass/v2/developerguide/ipc-iot-core-mqtt.html). Example message:
+
+```
+{
+  "Indoor Temperature": 27.2,
+  "Indoor Humidity": 46,
+  "Absolute Pressure": 1008.2,
+  "Relative Pressure": 1008.2,
+  "Outdoor Temperature": 32.5,
+  "Outdoor Humidity": 43,
+  "Wind Direction": 74,
+  "Wind Speed": 0.5,
+  "Gust Speed": 0.9,
+  "Light": 59800.0,
+  "UV": 1400,
+  "UV Index": 4,
+  "Soil Moisture 1": 35,
+  "Soil Moisture 3": 4,
+  "Temperature 1": 26.5,
+  "Humidity 1": 54,
+  "Temperature 2": 28.4,
+  "Humidity 2": 46,
+  "Temperature 3": 27.4,
+  "Humidity 3": 50,
+  "Temperature 4": 26.8,
+  "Humidity 4": 45,
+  "Temperature 5": 31.1,
+  "Maximum Wind Speed": 5.9
+}
+```
+
+## Repository Contents
+
+| Item                          | Description                                                         |
+| ----------------------------- | ------------------------------------------------------------------- |
+| gdk-config.json               | Configuration for the Greengrass Development Kit (GDK) - Command Line Interface.                      |
+| main.py                       | Greengrass component artifact source code.                          |
+| recipe.yaml                   | Greengrass component recipe.                                        |
+
+## Configuration
+
+The component supports the following [component configuration](https://docs.aws.amazon.com/greengrass/v2/developerguide/update-component-configurations.html) options.
+
+| Name              | Default                                              | Description |
+| ----------------- | ---------------------------------------------------- | ----------- |
+| gatewayAddress    | 0.0.0.0                                              | The IP address of the Ecowitt gateway. |
---
+++ main.py
@@ -0,0 +1,120 @@
+"""
+AWS IoT Greengrass component for publishing Ecowitt weather data to AWS IoT Core
+"""
+
+import json
+import socket
+import time
+import traceback
+from awsiot.greengrasscoreipc.clientv2 import GreengrassCoreIPCClientV2
+from awsiot.greengrasscoreipc.model import QOS
+
+PORT = 45000
+CMD_LIVEDATA = b'\xff\xff\x27\x03\x2a'
+TOPIC = 'ecowitt/livedata'
+
+# Taken from EcoWitt Data Exchange TCP Protocol document
+# Suitable for GW1000, 1100, 1900, 2000, 2001, 2680, 2650
+# Tested with GW2000C_V3.0.9, 4xWH31, 1xWN36 and 3xWH51
+# Solar Irradiance = Lux x 0.0079 W/m2
+LIVE_DATA_ITEMS = {
+    #    name                   size    divisor
+    1:  ('Indoor Temperature',  2,      10),
+    2:  ('Outdoor Temperature', 2,      10),
+    6:  ('Indoor Humidity',     1,      1),
+    7:  ('Outdoor Humidity',    1,      1),
+    8:  ('Absolute Pressure',   2,      10), # hPa
+    9:  ('Relative Pressure',   2,      10),
+    10: ('Wind Direction',      2,      1),  # degrees
+    11: ('Wind Speed',          2,      10), # m/s
+    12: ('Gust Speed',          2,      10),
+    21: ('Light',               4,      10), # Lux
+    22: ('UV',                  2,      1),  # uW/m2
+    23: ('UV Index',            1,      1),  # 0-15
+    25: ('Maximum Wind Speed',  2,      10),
+    26: ('Temperature 1',       2,      10),
+    27: ('Temperature 2',       2,      10),
+    28: ('Temperature 3',       2,      10),
+    29: ('Temperature 4',       2,      10),
+    30: ('Temperature 5',       2,      10),
+    31: ('Temperature 6',       2,      10),
+    32: ('Temperature 7',       2,      10),
+    33: ('Temperature 8',       2,      10),
+    34: ('Humidity 1',          1,      1),
+    35: ('Humidity 2',          1,      1),
+    36: ('Humidity 3',          1,      1),
+    37: ('Humidity 4',          1,      1),
+    38: ('Humidity 5',          1,      1),
+    39: ('Humidity 6',          1,      1),
+    40: ('Humidity 7',          1,      1),
+    41: ('Humidity 8',          1,      1),
+    44: ('Soil Moisture 1',     1,      1),
+    46: ('Soil Moisture 2',     1,      1),
+    48: ('Soil Moisture 3',     1,      1),
+    50: ('Soil Moisture 4',     1,      1),
+    52: ('Soil Moisture 5',     1,      1),
+    54: ('Soil Moisture 6',     1,      1),
+    56: ('Soil Moisture 7',     1,      1),
+    58: ('Soil Moisture 8',     1,      1),
+}
+
+def parse_live_data(data):
+    """ Parse live data from EcoWitt device """
+    msg = {}
+    payload_length = (data[3] * 256) + data[4]
+    checksum = sum(data[2:-1]) & 0xFF
+
+    # Only proceed if the response is valid and expected
+    if data[0] == 0xff and data[1] == 0xff and data[2] == 0x27 and\
+        payload_length == (len(data) - 2) and checksum == data[-1]:
+
+        index = 5
+
+        while index < (len(data) - 1):
+            if data[index] in LIVE_DATA_ITEMS:
+                value_payload = data[index + 1:index + LIVE_DATA_ITEMS[data[index]][1] + 1]
+                value = int.from_bytes(value_payload, byteorder='big')
+                if LIVE_DATA_ITEMS[data[index]][2] != 1:
+                    value /= LIVE_DATA_ITEMS[data[index]][2]
+                msg[LIVE_DATA_ITEMS[data[index]][0]] = value
+                index += LIVE_DATA_ITEMS[data[index]][1] + 1
+            else:
+                break
+    else:
+        print('Invalid or expected response from Ecowitt')
+
+    return msg
+
+ipc_client = None
+
+while ipc_client is None:
+    try:
+        print('Creating IPC client')
+        ipc_client = GreengrassCoreIPCClientV2()
+    except Exception as error:
+        print(f'Error creating IPC client: {repr(error)}.')
+
+configuration = ipc_client.get_configuration()
+print(f'Configuration: {configuration}')
+
+while True:
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((configuration.value['gatewayAddress'], PORT))
+            s.settimeout(3.0)
+
+            s.sendall(CMD_LIVEDATA)
+            live_data = s.recv(1024)
+            iot_msg = parse_live_data(live_data)
+            print(iot_msg)
+
+            s.close()
+
+            ipc_client.publish_to_iot_core(topic_name=TOPIC, qos=QOS.AT_MOST_ONCE,\
+                                           payload=json.dumps(iot_msg))
+
+    except Exception as e:
+        traceback.print_exc()
+        print(e)
+
+    time.sleep(60)
